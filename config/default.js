cf788abeb6052363e46cc489fdf6335ed0fd4cb9
Julie Carra <julie.carra@live.fr>
2020-04-27 13:24:15 +0200

    make it better

--- config/default.js
+++ config/default.js
@@ -1,4 +1,5 @@
 // Define your configurations
 module.exports = {
-  MONGO_URI: "mongodb://localhost:27017/r-meetup"
+  MONGO_URI:
+    "mongodb+srv://Julie:azerty@cluster0-oraqm.mongodb.net/test?retryWrites=true&w=majority",
 };
